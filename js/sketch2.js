f603eaf0d54414ae368b985b67153f2804a8bf66
yomandrake <yomandrake@gmail.com>
2020-04-28 15:25:00 +0200

    First Upload of github web page. It is a test with p5 Js simple sketch

---
+++ js/sketch2.js
@@ -0,0 +1,35 @@
+let sketch = function(p) {
+
+
+  p.setup = function() {
+    p.background(0);
+    p.createCanvas(700, 410);
+  };
+
+  p.draw = function() {
+    let r = 100;  
+    let g = p.map(p.mouseX,0,p.width,0,255,true);
+    let b = p.map(p.mouseY,0,p.height,0,255,true);
+    p.background(0,20);
+    p.fill(r,g,b);
+    p.rect(p.mouseX, p.mouseY, 50, 50);
+  };
+};
+
+let sketch2 = function(p) {
+  let x = 100;
+  let y = 100;
+
+  p.setup = function() {
+    p.createCanvas(700, 410);
+  };
+
+  p.draw = function() {
+    p.background(0);
+    p.fill(255,120,50);
+    p.rect(p.mouseX, p.mouseY, 50, 50);
+  };
+};
+
+new p5(sketch, 'container');
+new p5(sketch2, 'container2');
