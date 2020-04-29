6c1025149ad07267b1ace9133e90855b0e164a29
yomandrake <yomandrake@gmail.com>
2020-04-29 20:33:10 +0200

    Adding some routes
    -Adding another component to add route example
    -Add Page not found
    -Add mismatch defualt route to redirect to home

--- mdkWebAngular/src/app/app-routing.module.ts
+++ mdkWebAngular/src/app/app-routing.module.ts
@@ -1,8 +1,15 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
+import { HomePageComponent } from './home-page/home-page.component';
+import { CirclesComponent } from './circles/circles.component';
+import { PageNotFoundComponent } from './page-not-found/page-not-found.component';
 
-
-const routes: Routes = [];
+const routes: Routes = [
+  { path: 'home', component: HomePageComponent },
+  { path: 'circles', component: CirclesComponent },
+  { path: '',   redirectTo: '/home', pathMatch: 'full' },
+  { path: '**', component: PageNotFoundComponent }
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
--- mdkWebAngular/src/app/app.module.ts
+++ mdkWebAngular/src/app/app.module.ts
@@ -4,11 +4,15 @@ import { NgModule } from '@angular/core';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { HomePageComponent } from './home-page/home-page.component';
+import { CirclesComponent } from './circles/circles.component';
+import { PageNotFoundComponent } from './page-not-found/page-not-found.component';
 
 @NgModule({
   declarations: [
     AppComponent,
-    HomePageComponent
+    HomePageComponent,
+    CirclesComponent,
+    PageNotFoundComponent
   ],
   imports: [
     BrowserModule,
